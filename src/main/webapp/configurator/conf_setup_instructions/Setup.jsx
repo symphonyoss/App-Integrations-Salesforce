9d3b3535343b32d35b7d38a68ceb1e598f0233f3
Robson Vinícius Vieira Sanchez <rsanchez@symphony.com>
2017-03-03 09:48:01 -0300

    APP-808 Package and run the configurator app together with the Integration Bridge

---
+++ src/main/webapp/configurator/conf_setup_instructions/Setup.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.css';
+import './js/scripts.js';
+export default class Setup extends React.Component {
+	render() {
+		return(
+			<div>
+				{/* Start editing area */}
+				<p>Please contact your Symphony administrator to activate and customize your Salesforce.com integration.</p>
+				{/* End editing area */}
+			</div>
+		);
+	}
+}
---
+++ src/main/webapp/configurator/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	app_title: "Configure Salesforce",
+	app_name: "Salesforce",
+	IM_shorthand: "Salesforce", // will display "One on One with Sales Force"
+	toogleSetupInstructions: false
+}
