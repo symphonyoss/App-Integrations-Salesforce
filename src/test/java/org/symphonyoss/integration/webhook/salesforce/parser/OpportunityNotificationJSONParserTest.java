f1483b17be41c6556790c9428b32ba1f544a0c4f
Cassiano Repache <crepache@daitangroup.com>
2017-03-29 17:03:00 -0300

    Unit test about OpportunityNotification without Account and Owner.

--- src/test/java/org/symphonyoss/integration/webhook/salesforce/parser/OpportunityNotificationJSONParserTest.java
+++ src/test/java/org/symphonyoss/integration/webhook/salesforce/parser/OpportunityNotificationJSONParserTest.java
@@ -34,17 +34,15 @@ public class OpportunityNotificationJSONParserTest extends BaseSalesforceTest {
   private static final String CONTENT_TYPE_HEADER_PARAM = "content-type";
   private static final String OPPORTUNITY_NOTIFICATION = "SFDCCallbackSampleOpportunity.json";
   private static final String OPPORTUNITY_NOTIFICATION_WITHOUT_NEXT_STEPS = "SFDCCallbackSampleOpportunity_WithoutNextStep.json";
-  private static final String OPPORTUNITY_NOTIFICATION_WITH_ALL_FIELDS_NULL = "SFDCCallbackSampleOpportunity_WithAllFieldsNull.json";
   private static final String OPPORTUNITY_NOTIFICATION_WITHOUT_AMOUNT = "SFDCCallbackSampleOpportunity_WhitoutAmount.json";
+  private static final String OPPORTUNITY_NOTIFICATION_WITH_ALL_FIELDS_NULL = "SFDCCallbackSampleOpportunity_WithAllFieldsNull.json";
+  private static final String OPPORTUNITY_NOTIFICATION_WITHOUT_TAGS_ACCOUNT_AND_OWNER = "SFDCCallbackSampleOpportunity_WithoutTagsAccountAndOwner.json";
 
-  public static final String PARSER_OPPORTUNITY_NOTIFICATION_JSON_WITHOUT_AMOUNT =
-      "parser/opportunityNotificationJSON_WithoutAmount";
-  public static final String PARSER_OPPORTUNITY_NOTIFICATION_JSON_WITHOUT_ALL_FIELDS_NULL =
-      "parser/opportunityNotificationJSON_WithoutAllFieldsNull";
-  public static final String PARSER_OPPORTUNITY_NOTIFICATION_JSON_WITHOUT_NEXT_STEP =
-      "parser/opportunityNotificationJSON_withoutNextStep";
-  public static final String PARSER_OPPORTUNITY_NOTIFICATION_JSON =
-      "parser/opportunityNotificationJSON";
+  public static final String PARSER_OPPORTUNITY_NOTIFICATION_JSON = "parser/opportunityNotificationJSON";
+  public static final String PARSER_OPPORTUNITY_NOTIFICATION_JSON_WITHOUT_NEXT_STEP = "parser/opportunityNotificationJSON_WithoutNextStep";
+  public static final String PARSER_OPPORTUNITY_NOTIFICATION_JSON_WITHOUT_AMOUNT = "parser/opportunityNotificationJSON_WithoutAmount";
+  public static final String PARSER_OPPORTUNITY_NOTIFICATION_JSON_WITHOUT_ALL_FIELDS_NULL = "parser/opportunityNotificationJSON_WithoutAllFieldsNull";
+  public static final String PARSER_OPPORTUNITY_NOTIFICATION_JSON_WITHOUT_TAGS_ACCOUNT_AND_OWNER = "parser/opportunityNotificationJSON_WithoutTagsAccountAndOwner";
 
   @Mock
   private UserService userService;
@@ -108,6 +106,19 @@ public class OpportunityNotificationJSONParserTest extends BaseSalesforceTest {
 
     assertNotNull(result);
     String expected = readFile(PARSER_OPPORTUNITY_NOTIFICATION_JSON_WITHOUT_AMOUNT);
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void testOpportunityNotificationWithoutTagsAccountAndOwner() throws JAXBException, IOException {
+    JsonNode node = readJsonFromFile(OPPORTUNITY_NOTIFICATION_WITHOUT_TAGS_ACCOUNT_AND_OWNER);
+    Map<String, String> headerParams = new HashMap<>();
+    headerParams.put(CONTENT_TYPE_HEADER_PARAM, MediaType.APPLICATION_JSON);
+
+    String result = salesforceParser.parse(Collections.<String, String>emptyMap(), node);
+
+    assertNotNull(result);
+    String expected = readFile(PARSER_OPPORTUNITY_NOTIFICATION_JSON_WITHOUT_TAGS_ACCOUNT_AND_OWNER);
     assertEquals(expected, result);
   }
 
