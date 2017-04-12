31d75ac98ba601cbb7482654801ec342e93ff12b
pdarde-daitan <pdarde@daitangroup.com>
2017-04-12 13:41:45 -0300

    Salesforce update architecture

---
+++ src/main/webapp/components/SetupInstructions/template.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable global-require */
+import React from 'react';
+
+const Instructions = () => (
+  <div>
+    <p>
+      Please contact your Symphony administrator to activate and customize your Salesforce.com
+      integration.
+    </p>
+  </div>
+);
+
+export default Instructions;
---
+++ src/main/webapp/js/config.service.js
@@ -0,0 +1,7 @@
+export default ({
+  appTitle: 'Configure Salesforce',
+  appName: 'Salesforce',
+  appSubTitle: 'Salesforce Webhook Integration',
+  appIMShortHand: 'Salesforce', // will display "One on One with Salesforce"
+  appLogo: 'salesforce_logo.png',
+});
